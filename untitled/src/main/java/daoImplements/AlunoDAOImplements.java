a736d7420e2d95f32eb53b7f23b5806ef55c668a
amorimfe7 <amorimfe7@gmail.com>
2026-05-07 20:35:42 -0300

    implementando método Listar

--- untitled/src/main/java/daoImplements/AlunoDAOImplements.java
+++ untitled/src/main/java/daoImplements/AlunoDAOImplements.java
@@ -1,24 +1,49 @@
 package daoImplements;
 
 import dao.IAlunoDAO;
+import database.sqlConn;
 import model.Aluno;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AlunoDAOImplements implements IAlunoDAO {
 
     @Override
-    public void salvarAluno(Aluno aluno){};
+    public void salvarAluno(Aluno aluno){}
 
     @Override
     public void criar(Aluno aluno) {
-
     }
 
     @Override
     public List<Aluno> listarTodosAlunos() {
-        return List.of();
-    }
+        String sql = "SELECT * FROM aluno ORDER BY nome ASC";
+        List<Aluno> alunos = new ArrayList<>();
+
+        try(Connection conn = sqlConn.getConnection()){
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()){
+                alunos.add(new Aluno(
+                        rs.getInt("id"),
+                        rs.getString("nome"),
+                        rs.getString("cpf"),
+                        rs.getString("email"),
+                        rs.getDate("data_nascimento").toLocalDate(),
+                        rs.getString("telefone")
+                ));
+            }
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+        return alunos;
+    };
 
     @Override
     public void atualizarAluno(Aluno aluno) {
